1852748a859aee6e480ceb878a8ab051c6671814
abhishekhaldar <haldarabhishek64@gmail.com>
2025-06-05 21:33:17 +0530

    added a route

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ import { test } from './index.js';
 const app = express();
 const port = 8000;
 
-app.get('/hii', (req, res) => {
+app.use('/hii', (req, res) => {
     test();
     res.send('Hello, World!');
 });
 
-app.get('/name',(req,res)=>{
+app.use('/name',(req,res)=>{
     res.status(200).json({message:"hii abhishek", status:true})
 })
 
